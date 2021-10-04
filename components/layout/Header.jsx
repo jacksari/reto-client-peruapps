812c1c9e3c93cf55ab99b036a9481d4ebe155471
Jack Sari <janasarii@gmail.com>
2021-10-03 23:51:19 -0500

    add responsive

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import authContext from "../../context/auth/authContext";
+import Link from "next/link";
 
 function Header({title, description}) {
-    const { usuario } = useContext(authContext)
+    const { usuario, logout } = useContext(authContext)
+    const [sidebar, setSidebar] = useState(false);
     return (
         <div className="header">
             <div className="info">
@@ -10,6 +12,19 @@ function Header({title, description}) {
                 <h4>{ description }</h4>
             </div>
             <div className="user">
+                <div className="menu">
+                    <i onClick={()=>setSidebar(!sidebar)} className="fas fa-bars"/>
+                    <p>Menú</p>
+                </div>
+                <div className={sidebar ? 'sidebar-mobile active' : 'sidebar-mobile'}>
+                    <Link href="/dashboard">
+                        <a>1. Home</a>
+                    </Link>
+                    <Link href="/dashboard/create">
+                        <a>2. Crear usuario</a>
+                    </Link>
+                    <button onClick={logout} className="btn">Logout</button>
+                </div>
                 <div className="name">
                     <p className="user">{ usuario ? usuario.name : '' }</p>
                     <p className="email">{ usuario ? usuario.email : '' }</p>
